3c964d01064681e3af189b0a167edbbf35176085
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-06-26 13:27:21 +0530

    Add mechanism to name nodes and toggling unnamed nodes

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 // To prevent default behavior on right-mouse-button click
-document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener("contextmenu", (event) => event.preventDefault());
 
 // Fetch the input field for the vertex-name
 const vertexNameTextInput = document.getElementById("vertexName");
@@ -22,12 +22,15 @@ let circleList = [];
 let edgeList = [];
 
 let shortestPath = null;
-const vertexRadius = 5; 
+const vertexRadius = 5;
 
 // To hide edges, nodes or both
 let HIDE_ALL = false;
 let HIDE_EDGES = false;
-let HIDE_NODES = false;
+let HIDE_UNNAMED_NODES = false;
+
+// To name the nodes
+let NODE_NAMING_MODE = false;
 
 /*
   Util functions
@@ -37,19 +40,17 @@ let HIDE_NODES = false;
  * Returns the sum of object's attribute
  * @param {*} object The object containing some attribute
  * @param {string} attribute The attribute to sum (in string)
- * @returns 
-*/
+ * @returns
+ */
 const sum = (object, attribute) => {
-  
   let _sum = object[0][attribute];
-  
-  for(let i = 1; i < object.length; i++) {
+
+  for (let i = 1; i < object.length; i++) {
     _sum = _sum + object[i][attribute];
   }
-  
-  return _sum;
-}
 
+  return _sum;
+};
 
 /*
 Functions for different modes
@@ -61,47 +62,50 @@ Functions for different modes
  * @param {int} y The y coordinate of vertex
  */
 const createVertex = (x, y) => {
-
   // Check if we've clicked on other circle
-  for(let circle of circleList) {
-    if(circle.isPointWithin(x, y, circle.radius)) {
+  for (let circle of circleList) {
+    if (circle.isPointWithin(x, y, circle.radius)) {
       return;
     }
   }
 
   // Check if we've clicked near the boundary
-  if(x < vertexRadius || x > WIDTH - vertexRadius) return;
-  if(y < vertexRadius || y > HEIGHT - vertexRadius) return;
+  if (x < vertexRadius || x > WIDTH - vertexRadius) return;
+  if (y < vertexRadius || y > HEIGHT - vertexRadius) return;
 
   // Else create a circle on that point
-  circleList.push( new Circle(x, y, vertexRadius) );
-}
+  circleList.push(new Circle(x, y, vertexRadius));
+};
 
 /**
  * Appends an edge to the edgeList
  * @param {*} x The x coordinate of vertex
  * @param {*} y The y coordinate of vertex
- * @returns 
+ * @returns
  */
 const createEdge = (currentCircle) => {
-
   // Check if two circles are highlighted
-  if(sum(circleList, "isHighlighted") === 2) {
+  if (sum(circleList, "isHighlighted") === 2) {
     // Make both the circles un-highlighted
     edgeList[edgeList.length - 1][0].isHighlighted = false;
     currentCircle.isHighlighted = false;
 
     // Don't create the edge if it already exists
-    for(let edge of edgeList) {
-
+    for (let edge of edgeList) {
       // Check if the edge A to B exists
-      if(edge[0] === edgeList[edgeList.length-1][0] && edge[1] === currentCircle) {
+      if (
+        edge[0] === edgeList[edgeList.length - 1][0] &&
+        edge[1] === currentCircle
+      ) {
         edgeList.pop();
         return;
       }
 
       // Check if the edge B to A exists
-      if(edge[0] === currentCircle && edge[1] === edgeList[edgeList.length-1][0]) {
+      if (
+        edge[0] === currentCircle &&
+        edge[1] === edgeList[edgeList.length - 1][0]
+      ) {
         edgeList.pop();
         return;
       }
@@ -111,88 +115,83 @@ const createEdge = (currentCircle) => {
     edgeList[edgeList.length - 1][1] = currentCircle;
   } else {
     // Append an empty edge
-    edgeList.push( [currentCircle, null] );
+    edgeList.push([currentCircle, null]);
   }
-
-}
-
+};
 
 /**
  * Deletes a vertex from the canvas
  */
 const deleteVertex = (currentCircle) => {
-
   // Delete all the edges associated with it
-  edgeList = edgeList.filter(edge => {
+  edgeList = edgeList.filter((edge) => {
     return edge[0] !== currentCircle && edge[1] !== currentCircle;
-  })
+  });
 
   // Remove the vertex like this
-  circleList = circleList.filter(circle => {
+  circleList = circleList.filter((circle) => {
     return circle != currentCircle;
   });
-}
-
+};
 
 /**
  * Find the shortest path between the two vertices
  */
 const findPath = (lastCircle) => {
-
   // Fetch the two vertices
   // If there are more than 1 circle highlighted then there's something wrong
-  if(sum(circleList, "isHighlighted") !== 1) return;
+  if (sum(circleList, "isHighlighted") !== 1) return;
 
   // We need to remove the 'null' edge at the last of edgeList
-  edgeList.splice(edgeList.length-1, 1);
+  edgeList.splice(edgeList.length - 1, 1);
 
   let start = lastCircle;
   let goal = null;
-  
+
   // Find the second circle
-  for(let circle of circleList) {
-    if(circle.isHighlighted) {
+  for (let circle of circleList) {
+    if (circle.isHighlighted) {
       circle.isHighlighted = false;
       goal = circle;
       break;
     }
   }
 
-  console.log("start: " + start);
-  console.log("goal: " + goal);
-
-  const manhattanDistance = (vertex0, vertex1) => Math.abs(vertex0.x-vertex1.x) + Math.abs(vertex0.y-vertex1.y);
+  const manhattanDistance = (vertex0, vertex1) =>
+    Math.abs(vertex0.x - vertex1.x) + Math.abs(vertex0.y - vertex1.y);
 
   this.currentGraph = new Graph(circleList, edgeList);
   shortestPath = this.currentGraph.A_star(start, goal, manhattanDistance);
 
-
   // Then create a path string
-  let pathString = shortestPath[shortestPath.length-1].name;
-  for(let i = shortestPath.length-2; i >= 0; i--) {
-    if(shortestPath[i].name === null) continue;
+  let pathString = shortestPath[shortestPath.length - 1].name;
+  for (let i = shortestPath.length - 2; i >= 0; i--) {
+    if (shortestPath[i].name === null) continue;
 
     pathString = `${pathString} ${unicodeArrowRight} ${shortestPath[i].name}`;
   }
   directionParagraph.innerHTML = pathString;
-}
-
+};
 
 /**
  * Deletes all the edges and vertices
  */
 const clearAll = () => {
-    circleList = new Array();
-    edgeList = new Array();
-}
+  circleList = new Array();
+  edgeList = new Array();
+};
 
 /**
  * To hide all the nodes and edges
  */
-const toggleHideNodes = () => HIDE_NODES = !HIDE_NODES;
-const toggleHideEdges = () => HIDE_EDGES = !HIDE_EDGES;
-const toggleHideAll = () => HIDE_ALL = !HIDE_ALL;
+const toggleUnnamedNodes = () => (HIDE_UNNAMED_NODES = !HIDE_UNNAMED_NODES);
+const toggleHideEdges = () => (HIDE_EDGES = !HIDE_EDGES);
+const toggleHideAll = () => (HIDE_ALL = !HIDE_ALL);
 
+const toggleNamingMode = () => {
+  NODE_NAMING_MODE = !NODE_NAMING_MODE;
+  console.log("NAMING NODES: " + NODE_NAMING_MODE);
+};
 
 /**
  * Gives the name to the vertex
@@ -202,21 +201,25 @@ const giveNameToVertex = () => {
   circleHighlighted = null;
 
   let i = 0;
-  for(; i < circleList.length; i++) {
-    if(circleList[i].isHighlighted) {
+  for (; i < circleList.length; i++) {
+    if (circleList[i].isHighlighted) {
       circleHighlighted = circleList[i];
       break;
     }
   }
   // If there are no circles highlighted then return
-  if(circleHighlighted === null) return;
+  if (circleHighlighted === null) return;
 
   // Make the circle un-highlighted
   circleHighlighted.isHighlighted = false;
 
   // Else assign the input field to the circle's name
-  circleHighlighted.name = vertexNameTextInput.value;
+  if (vertexNameTextInput.value === "") {
+    circleHighlighted.name = null;
+  } else {
+    circleHighlighted.name = vertexNameTextInput.value;
+  }
 
   // Remove the last extra edge created
-  edgeList.pop();
-}
+  if (!NODE_NAMING_MODE) edgeList.pop();
+};
--- static/sketch.js
+++ static/sketch.js
@@ -22,86 +22,105 @@ function draw() {
   image(bgImage, 0, 0, WIDTH, HEIGHT);
 
   // Draw the circle
-  if(!HIDE_NODES && !HIDE_ALL) {
-      stroke(0);
-      fill(0);
-      for(let c of circleList) {
-        // If the circle is highlighted then paint it red
-        if(c.isHighlighted) fill(255, 0, 0);
-        else fill(0);
-    
-        circle(c.x, c.y, c.radius*2);
-      }
+  if (!HIDE_ALL) {
+    stroke(0);
+    fill(0);
+    for (let c of circleList) {
+      // If the circle is unnamed and we aren't supposed to show them, then skip
+      if (c.name === null && HIDE_UNNAMED_NODES) continue;
+
+      // If the circle is highlighted then paint it red
+      if (c.isHighlighted) fill(255, 0, 0);
+      else fill(0);
+
+      circle(c.x, c.y, c.radius * 2);
+    }
   }
 
-  if(!HIDE_EDGES && !HIDE_ALL) {
+  if (!HIDE_EDGES && !HIDE_ALL) {
     // Draw the edges
     stroke(0, 0, 255);
-    for(let edge of edgeList) {
-      if(edge[1] === null) continue;
-  
+    for (let edge of edgeList) {
+      if (edge[1] === null) continue;
+
       // Draw a line
       line(edge[0].x, edge[0].y, edge[1].x, edge[1].y);
     }
   }
 
   // Draw the path
-  if(shortestPath === null) return;
-  
-  stroke(0, 255, 0);
-  strokeWeight(vertexRadius*0.85);
-  for(let i = 0; i < shortestPath.length-1; i++) {
-    // Draw a line
-    line(shortestPath[i].x, shortestPath[i].y, shortestPath[i+1].x, shortestPath[i+1].y);
-  }
+  if (shortestPath === null) return;
 
+  stroke(0, 255, 0);
+  strokeWeight(vertexRadius * 0.85);
+  for (let i = 0; i < shortestPath.length - 1; i++) {
+    // Draw a line
+    line(
+      shortestPath[i].x,
+      shortestPath[i].y,
+      shortestPath[i + 1].x,
+      shortestPath[i + 1].y
+    );
+  }
 }
 
 function mousePressed(event) {
   // If we're clicking outside the canvas then don't do anything
-  if(mouseX > WIDTH || mouseX < 0) return;
-  if(mouseY > HEIGHT || mouseY < 0) return;
+  if (mouseX > WIDTH || mouseX < 0) return;
+  if (mouseY > HEIGHT || mouseY < 0) return;
 
   // Check if the user right clicked
-  let isRightClick = (event.button === 2);
+  let isRightClick = event.button === 2;
 
   let x = mouseX;
   let y = mouseY;
-  
+
   // Check if we're clicking on a circle
   let circleClicked = null;
-  for(let circle of circleList) {
-    if(circle.pointCollides(x, y)) {
+  for (let circle of circleList) {
+    if (circle.pointCollides(x, y)) {
       circleClicked = circle;
       break;
     }
   }
 
   // If we didn't click on any circle then create a circle
-  if(circleClicked === null && !isRightClick) {
+  if (circleClicked === null && !isRightClick) {
     createVertex(x, y);
     return;
   }
 
   // Check if one circle is already highlighted and the user clicked with right muse button
-  if(sum(circleList, "isHighlighted") === 1 && isRightClick) {
+  if (sum(circleList, "isHighlighted") === 1 && isRightClick) {
     findPath(circleClicked);
     return;
   }
 
   // Else highlight the circle
-  if(!circleClicked.isHighlighted) {
+  if (!circleClicked.isHighlighted) {
     // Highlight the circle
     circleClicked.isHighlighted = true;
     // And load the circle's name to the text input
-    vertexNameTextInput.value = (circleClicked.name === null) ? "null" : circleClicked.name;
-    
+    vertexNameTextInput.value =
+      circleClicked.name === null ? "null" : circleClicked.name;
   } else {
     // If the circle was already highlighted, then delete it
     deleteVertex(circleClicked);
     return;
   }
 
+  // Check if there are two circles highlighted, then un-highlight the other one
+  if (sum(circleList, "isHighlighted") === 2 && NODE_NAMING_MODE) {
+    for (let circle of circleList) {
+      if (!circle.isHighlighted) continue;
+      if (circle.isEqual(circleClicked)) continue;
+      // Else un-highlight the circle
+      circle.isHighlighted = false;
+    }
+  }
+
+  // If we're not in naming mode, then draw an edge
+  if (NODE_NAMING_MODE) return;
 
   // Then create an edge
   createEdge(circleClicked);
