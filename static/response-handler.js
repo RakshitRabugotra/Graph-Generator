15c559a65af08934fc6cf6c6b6243b2d269e2039
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-06-26 13:26:32 +0530

    Reformat code

--- static/response-handler.js
+++ static/response-handler.js
@@ -1,73 +1,86 @@
-
 const saveProgress = () => {
+  // Converting JSON data to string
+  const object = {
+    vertices: new Array(),
+    edges: new Array(),
+  };
 
-    // Converting JSON data to string
-    const object = {
-        vertices: new Array(),
-        edges: new Array()
-    }
+  // If circleList is empty then don't save
+  if (circleList.length === 0) return;
 
-    // If circleList is empty then don't save
-    if(circleList.length === 0) return;
-  
-    // Add all the circles
-    for(const circle of circleList) {
-        object.vertices.push({
-            "x": circle.x,
-            "y": circle.y,
-            "radius": circle.radius,
-            "name": circle.name
-        })
-    }
-    // Add all the edges
-    for(const edge of edgeList) {
-        object.edges.push([
-            {"x": edge[0].x, "y": edge[0].y, "radius": edge[0].radius, "name": edge[0].name},
-            {"x": edge[1].x, "y": edge[1].y, "radius": edge[1].radius, "name": edge[1].name},
-        ])
-    }
+  // Add all the circles
+  for (const circle of circleList) {
+    object.vertices.push({
+      x: circle.x,
+      y: circle.y,
+      radius: circle.radius,
+      name: circle.name,
+    });
+  }
+  // Add all the edges
+  for (const edge of edgeList) {
+    if (edge[1] === null) continue;
 
-    // Send the JSON to the backend using fetch API
-    const options = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(object)
-    }
+    object.edges.push([
+      {
+        x: edge[0].x,
+        y: edge[0].y,
+        radius: edge[0].radius,
+        name: edge[0].name,
+      },
+      {
+        x: edge[1].x,
+        y: edge[1].y,
+        radius: edge[1].radius,
+        name: edge[1].name,
+      },
+    ]);
+  }
 
-    const fetchResult = fetch("save", options);
-}
+  // Send the JSON to the backend using fetch API
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(object),
+  };
 
+  const fetchResult = fetch("save", options);
+};
 
 const loadProgress = () => {
-    // Send a request to the backend 
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    const fetchResult = fetch("load", options);
-    
-    // After fetch is completed we can access the data
-    fetchResult
-    .then(result => result.json())
-    .then(jsonData => {
-        // Copy the data found to circle-list and edge-list
-        jsonData["vertices"].forEach(vertex => {
-            circleList.push( new Circle(vertex.x, vertex.y, vertex.radius, vertex.name) );
-        });
-        jsonData["edges"].forEach(edge => {
-            const circle1 = edge[0];
-            const circle2 = edge[1];
+  // Send a request to the backend
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const fetchResult = fetch("load", options);
 
-            edgeList.push(new Array(null, null));
+  // After fetch is completed we can access the data
+  fetchResult
+    .then((result) => result.json())
+    .then((jsonData) => {
+      // Copy the data found to circle-list and edge-list
+      jsonData["vertices"].forEach((vertex) => {
+        circleList.push(
+          new Circle(vertex.x, vertex.y, vertex.radius, vertex.name)
+        );
+      });
+      jsonData["edges"].forEach((edge) => {
+        const circle1 = edge[0];
+        const circle2 = edge[1];
 
-            circleList.forEach(circle => {
-                if(circle.isEqual(circle1)) edgeList[edgeList.length-1][0] = circle;
-                if(circle.isEqual(circle2)) edgeList[edgeList.length-1][1] = circle;
-            });
+        edgeList.push(new Array(null, null));
+
+        circleList.forEach((circle) => {
+          if (circle.isEqual(circle1))
+            edgeList[edgeList.length - 1][0] = circle;
+          if (circle.isEqual(circle2))
+            edgeList[edgeList.length - 1][1] = circle;
         });
+      });
     });
-}
+};
